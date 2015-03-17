e8ffd7307beafd5f4c846751d85d25ea5b79e7cd
Joshua.Dickerson <Joshua.Dickerson@dealer.com>
2015-03-17 14:07:16 -0400

    fizz buzz for euler

--- src/FizzBuzz.scala
+++ src/FizzBuzz.scala
@@ -6,12 +6,8 @@ object FizzBuzz {
   val buzz = "buzz"
 
   def main(args: Array[String]): Unit ={
-    val nums = (0 to 100).toList
-    for {num <- nums if num % 3 == 0 || num % 5 == 0}{
-      if( num % 3 == 0 && num % 5 == 0) println( num + " | " +fizz + buzz)
-        else if (num % 3 == 0) println( num + " | " +fizz)
-        else println( num + " | " +buzz)
-    }
+    val result = (0 until 1000).filter(x => x % 3 == 0 || x % 5 == 0).sum
+    println(result)
 
   }
 
